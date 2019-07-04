41c04015eece6925b5f4875c9866117cfcb80326
mstfakdgn <mustafa/akiler@gmail.com>
2019-07-04 11:09:34 +0300

    larticle shit

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Model\Articles;
+use Illuminate\Database\Seeder;
+
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Models\Article::class ,30)->create();
+    }
+}
